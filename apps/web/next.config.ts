ac8e0a7d6fd0d5b1cad66db9175bd26689af2c0b
babually <mmrishoally@gmail.com>
2026-05-10 18:10:41 +0300

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@darasa-lako/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,32 @@
+import { expo } from "@better-auth/expo";
+import { createPrismaClient } from "@darasa-lako/db";
+import { env } from "@darasa-lako/env/server";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+
+export function createAuth() {
+  const prisma = createPrismaClient();
+
+  return betterAuth({
+    database: prismaAdapter(prisma, {
+      provider: "postgresql",
+    }),
+
+    trustedOrigins: [
+      env.CORS_ORIGIN,
+      "darasa-lako://",
+      ...(env.NODE_ENV === "development"
+        ? ["exp://", "exp://**", "exp://192.168.*.*:*/**", "http://localhost:8081"]
+        : []),
+    ],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    plugins: [nextCookies(), expo()],
+  });
+}
+
+export const auth = createAuth();
