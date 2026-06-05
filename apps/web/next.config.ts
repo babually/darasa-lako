1a15ecb1c5a2f138f6fec1f7b9f822bba36f61c9
babually <mmrishoally@gmail.com>
2026-06-05 23:10:37 +0300

    fix nextj deployment config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,6 +4,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	typedRoutes: true,
 	reactCompiler: true,
+	experimental: {
+		serverMinification: false,
+	},
 };
 
 export default nextConfig;
